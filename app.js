37f3cb8c96e485e32d1df24e8cd2477b9e838039
Ian Hart <ianhart13@gmail.com>
2021-09-22 18:07:55 -0400

    js function for seconds hand created

--- app.js
+++ app.js
@@ -1,9 +1,18 @@
 // app.js
 
+const secondHand = document.querySelector('.second-hand');
+const minHand = document.querySelector('.min-hand');
+const hourHand = document.querySelector('.hour-hand');
+
 function setDate() {
 	const now = new Date();
 	const seconds = now.getSeconds();
-	const secondsDegrees = ((seconds / 60) * 360);
+	const minute = now.getMinutes();
+	const hour = now.getHours();
+
+	const secondsDegrees = ((seconds / 60) * 360) + 90;
+	secondHand.style.transform = `rotate(${secondsDegrees}deg)`;
+
 	console.log(seconds);
 }
 
